2feee863781d99cb712254b424d509bb4e4a4f8b
root <root@localhost.localdomain>
2018-10-27 20:44:38 -0600

    Objeto json creado

---
+++ code.js
@@ -0,0 +1,45 @@
+window.onload = Json;
+
+function Carga(objJson){
+    var tabla = document.getElementById("tabla");
+    var tr = document.createElement("tr");
+
+    var nombre = document.createElement("td");
+    nombre.innerHTML = objJson.nombre;
+    tr.appendChild(nombre);
+
+    var apellido = document.createElement("td");
+    apellido.innerHTML = objJson.apellido;
+    tr.appendChild(apellido);
+
+    var edad = document.createElement("td");
+    edad.innerHTML = objJson.edad;
+    tr.appendChild(edad);
+
+    var sexo = document.createElement("td");
+    sexo.innerHTML = objJson.sexo;
+    tr.appendChild(sexo);
+
+    var domicilio = document.createElement("td");
+    domicilio.innerHTML = objJson.domicilio;
+    tr.appendChild(domicilio);
+
+    var nacionalidad = document.createElement("td");
+    nacionalidad.innerHTML = objJson.nacionalidad.value;
+    tr.appendChild(nacionalidad);
+
+    var tel = document.createElement("td");
+    tel.innerHTML = objJson.tel;
+    tr.appendChild(tel);
+
+    tabla.appendChild(tr);
+}
+
+function Json(){
+    var objJson = {"nombre":"Jonathan", "apellido":"Velasquez", "edad":"20", "sexo":"M",
+                    "domicilio":"El Viejo", "nacionalidad":"Nicaragua", "tel":"86691652"};
+
+    for(var i=0; i<5; i++){
+        Carga(objJson);
+    }
+}
